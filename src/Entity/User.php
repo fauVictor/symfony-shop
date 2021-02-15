9849e93d86a143c3858c21766d3936617b9d4314
victor <fau.victor@outlook.com>
2021-02-15 10:26:19 +0100

    update cicd on github

---
+++ src/Entity/User.php
@@ -0,0 +1,242 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+
+use DateTimeInterface;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     * 
+     * @var integer the id
+     */
+    private int $id;
+
+    /**
+     * @ORM\Column(type="string", length=180, unique=true)
+     * 
+     * @var string the email
+     */
+    private string $email;
+    
+    /**
+     * @ORM\Column(type="string")
+     * 
+     * @var string the hashed password
+     */
+    private string $password;
+
+    /**
+     * @ORM\Column(type="json")
+     * @var array<string> The list of roles
+     */
+    private Array $roles = [];
+
+    /**
+     * @ORM\Column(type="string")
+     * 
+     * @var string the civility
+     */
+    private string $civility;
+
+    /**
+     * @ORM\Column(type="string")
+     * 
+     * @var string the firstname
+     */
+    private string $firstname;
+
+    /**
+     * @ORM\Column(type="string")
+     * 
+     * @var string the lastname
+     */
+    private string $lastname;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private DateTimeInterface $updatedAt;
+
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    // email
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+    
+    // password
+    public function getPassword(): string
+    {
+        return (string) $this->password;
+    }
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the value of civility
+     *
+     * @return string
+     */
+    public function getCivility() : string 
+    {
+        return $this->civility;
+    }
+
+    /**
+     * @param string $civility
+     * @return self
+     */
+    public function setCivility(string $civility) : self
+    {
+        $this->civility = $civility;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastname() : string 
+    {
+        return $this->lastname;
+    }
+
+    /**
+     * @param string $lastname
+     * @return self
+     */
+    public function setLastname(string $lastname) : self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstname() : string 
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * @param string $firstname
+     * @return self
+     */
+    public function setFirstname(string $firstname) : self
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdentity() : string
+    {
+        return $this->civility . ' ' . $this->firstname . ' ' . $this->lastname;
+    }
+
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUsername(): string
+    {
+        return (string) $this->email;
+    }
+    /**
+     * @return array<string>
+     */
+    public function getRoles(): array
+    {
+        $roles = $this->roles;
+        $roles[] = 'ROLE_USER';
+
+        return array_unique($roles);
+    }
+
+    /**
+     * @param array<string> $roles
+     */
+    public function setRoles(array $roles = ['ROLE_USER']): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function getSalt(): ?string
+    {
+        return null;
+    }
+
+    /**
+     * @see UserInterface
+     */
+    public function eraseCredentials():void
+    {
+
+    }
+}
